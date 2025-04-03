9a83f30f22fc10702f6ddf5a4e6dab0940da123c
nghyeokSim <simjeonghyeok1020@gmail.com>
2025-04-03 21:02:43 +0900

    챕터3-3 10번문제 삼항연산자

---
+++ src/problemCh3/Problem10.java
@@ -0,0 +1,21 @@
+package problemCh3;
+
+public class Problem10 {
+    public static void main(String[] args) {
+
+        int num = -2;
+        String str = (num > 0) ? "양수" : (num == 0) ? "0" : "음수";
+        System.out.println(str);
+
+
+
+
+//        if (num > 0) {
+//            System.out.println("양수입니다");
+//        } else if (num == 0) {
+//            System.out.println("0입니다.");
+//        } else {
+//            System.out.println("음수입니다");
+//        }
+    }
+}
