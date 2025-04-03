d659a6e909935fc48d3689bf7fdbbb813ff8f3cf
nghyeokSim <simjeonghyeok1020@gmail.com>
2025-04-03 21:26:01 +0900

    챕터3-5 10페이지

---
+++ src/problemCh3/Problem3_5.java
@@ -0,0 +1,10 @@
+package problemCh3;
+
+public class Problem3_5 {
+    public static void main(String[] args) {
+
+        int num = 1345;
+        int a = num / 10 * 10 + 1;
+        System.out.println(a);
+    }
+}
