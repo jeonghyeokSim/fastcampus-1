35e0135d672553a938484cc661276981e8e316b1
nghyeokSim <simjeonghyeok1020@gmail.com>
2025-04-03 21:13:47 +0900

    챕터3-4 10페이지

---
+++ src/problemCh3/Problem3_4.java
@@ -0,0 +1,14 @@
+package problemCh3;
+
+public class Problem3_4 {
+    public static void main(String[] args) {
+        int num = 423;
+        int a = (num / 100) * 100;
+        System.out.println(a);
+//        if (num == 456) {
+//            System.out.println(400);
+//        } else if (num == 111)
+//            System.out.println(100);
+    }
+}
+
